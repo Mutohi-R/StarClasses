a5e487df8c5e7e32cda5475e4031dde8f1405304
Mutohir <mutohirokuniyi@gmail.com>
2024-11-25 17:39:30 +0100

    made accordion functional

--- src/components/Courses/AccordionItem.jsx
+++ src/components/Courses/AccordionItem.jsx
@@ -1,16 +1,23 @@
 import styles from './Courses.module.css'
 import classNames from 'classnames'
+import PropTypes from 'prop-types'
 import caret from "@icons/caret.svg";
+import { useState } from 'react';
 
 
-const AccordionItem = ({ icon, title }) => {
+const AccordionItem = ({ icon, title, children }) => {
+
+  const [isAccordionOpen, setIsAccordionOpen] = useState(true);
+  const toggleAccordion = () => setIsAccordionOpen(!isAccordionOpen);
+
   return (
-    <div className={classNames(styles.accordionItem)}>
+    <div className={classNames(styles.accordionItem, "grid gap-3")}>
       <div
         className={classNames(
           "flex align-items-center gap-2 justify-between",
           styles.accordionHeader
         )}
+        onClick={toggleAccordion}
       >
         <div className="flex align-items-center gap-2">
           <div>
@@ -18,12 +25,21 @@ const AccordionItem = ({ icon, title }) => {
           </div>
           <p>{title}</p>
         </div>
-        <button className={styles.caret}>
+        <button data-open={isAccordionOpen} className={styles.caret}>
           <img src={caret} alt="" />
         </button>
       </div>
+      <div data-open={isAccordionOpen} className={classNames(styles.accordionContent)}>
+        {children}
+      </div>
     </div>
   );
+}
+
+AccordionItem.propTypes = {
+  icon: PropTypes.string,
+  title: PropTypes.string,
+  children: PropTypes.node
 }
 
 export default AccordionItem
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -16,25 +16,102 @@ const Courses = () => {
     <section className='breakout'>
       <article className={classNames(styles.accordion, "br-1")}>
         <div className='grid gap-2'>
-            <AccordionItem  icon={mathematics} title="Mathematics" />
+            <AccordionItem  icon={mathematics} title="Mathematics" >
+              <ul role='list'>
+                <li>Algebra</li>
+                <li>Calculus</li>
+                <li>Geometry</li>
+                <li>Trigonometry</li>
+                <li>Statistics & probability</li>
+                <li>Advanced mathematics (for college prep)</li>
+              </ul>
+            </AccordionItem>
             <Line />
-            <AccordionItem icon={commerce} title="Commerce" />
+            <AccordionItem icon={commerce} title="Commerce">
+              <ul role='list'>
+                <li>Accounting</li>
+                <li>Financial management</li>
+                <li>Marketing principles</li>
+                <li>Business studies</li>
+                <li>Entrepreneurship</li>
+                <li>Economics</li>
+              </ul>
+            </AccordionItem>
             <Line />
-            <AccordionItem icon={languages} title="Languages" />
+            <AccordionItem icon={languages} title="Languages">
+              <ul role='list'>
+                <li>English language & literature</li>
+                <li>Spanish</li>
+                <li>French</li>
+                <li>German</li>
+                <li>Chinese</li>
+                <li>Japanese</li>
+                <li>Public speaking & communication skills</li>
+                <li>Creative writing</li>
+              </ul>
+            </AccordionItem>
             <Line />
-            <AccordionItem icon={computerScience} title="Computer Science" />
+            <AccordionItem icon={computerScience} title="Computer Science">
+              <ul role='list'>
+                <li>Introduction to programming (Python)</li>
+                <li>Web development basics (HTML, CSS, Javascript)</li>
+                <li>Data science fundamentals</li>
+                <li>Artificial intelligence for beginners</li>
+                <li>Computer applications in business</li>
+                <li>Data structures and algorithms</li>
+                <li>Digital literacy skills</li>
+                <li>Programming practices and techniques</li>
+              </ul>
+            </AccordionItem>
             <div className={styles.extraLine}>
                 <Line />
             </div>
         </div>
         <div className='grid gap-2'>
-            <AccordionItem icon={testPrep} title="Test Prep" />
+            <AccordionItem icon={testPrep} title="Test Prep">
+              <ul role='list'>
+                <li>SAT preparation</li>
+                <li>ACT preparation</li>
+                <li>GRE quantitative</li>
+                <li>GMAT verbal and quantitative</li>
+                <li>AP exam prep</li>
+                <li>IELTS/TOEFL english prep</li>
+              </ul>
+            </AccordionItem>
             <Line />
-            <AccordionItem icon={fineArts} title="Fine Arts" />
+            <AccordionItem icon={fineArts} title="Fine Arts">
+              <ul role='list'>
+                <li>Visual arts</li>
+                <li>Music theory</li>
+                <li>Photography basics</li>
+                <li>Dance and movement</li>
+                <li>Graphic design fundamentals</li>
+                <li>Theatre & performance arts</li>
+              </ul>
+            </AccordionItem>
             <Line />
-            <AccordionItem icon={personalDevelopment} title="Personal Development" />
+            <AccordionItem icon={personalDevelopment} title="Personal Development">
+              <ul role="list">
+                <li>Time management</li>
+                <li>Leadership skills</li>
+                <li>Critical thinking and problem solving</li>
+                <li>Emotional intelligence</li>
+                <li>Career counselling & resume building</li>
+                <li>Stress management for students</li>
+              </ul>
+            </AccordionItem>
             <Line />
-            <AccordionItem icon={technology} title="Technology" />
+            <AccordionItem icon={technology} title="Technology">
+              <ul role='list'>
+                <li>Introduction to cybersecurity</li>
+                <li>Cloud computing basics</li>
+                <li>Networking fundamentals</li>
+                <li>Internet of things (IoT)</li>
+                <li>Blockchain technology</li>
+                <li>Quantum computing</li>
+                <li>Augmented reality (AR) & virtual reality (VR)</li>
+              </ul>
+            </AccordionItem>
         </div>
       </article>
     </section>
