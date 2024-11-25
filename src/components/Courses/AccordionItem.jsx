e727e62fce76e0359d09707b16610863fb30d566
Mutohir <mutohirokuniyi@gmail.com>
2024-11-25 17:46:13 +0100

    made accordion more functional

--- src/components/Courses/AccordionItem.jsx
+++ src/components/Courses/AccordionItem.jsx
@@ -2,13 +2,27 @@ import styles from './Courses.module.css'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import caret from "@icons/caret.svg";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 const AccordionItem = ({ icon, title, children }) => {
 
-  const [isAccordionOpen, setIsAccordionOpen] = useState(true);
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
   const toggleAccordion = () => setIsAccordionOpen(!isAccordionOpen);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setIsAccordionOpen(true);
+      } else {
+        setIsAccordionOpen(false);
+      }
+    }
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+  }, [])
+  
 
   return (
     <div className={classNames(styles.accordionItem, "grid gap-3")}>
